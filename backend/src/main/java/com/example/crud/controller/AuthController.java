548b0f29d4a5429ce9fe799e28ff7503eebb2d5e
Developer <dev@localhost>
2025-11-27 15:19:49 +0530

    Fix: Refactored AuthController to use Lombok and resolve unused import warning

--- backend/src/main/java/com/example/crud/controller/AuthController.java
+++ backend/src/main/java/com/example/crud/controller/AuthController.java
@@ -46,24 +46,9 @@ public class AuthController {
                 "role", user.getRole()));
     }
 
+    @Data
     public static class LoginRequest {
         private String username;
         private String password;
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
     }
 }
